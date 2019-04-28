90ba8ef592641ec2ab67e596b1e945f9e631a03b
marks <markswearingen@bendcable.com>
2019-04-27 19:52:37 -0700

    challenge 6 before 4

--- code-challenges/301/regex04/challenges-04.test.js
+++ code-challenges/301/regex04/challenges-04.test.js
@@ -76,7 +76,9 @@ For example, 'Welcome to Code 301!' will return 'W_lc_m_ t_ C_d_ 301!'.
 ------------------------------------------------------------------------------------------------ */
 
 let hangman = (str) => {
-  // Solution code here...
+  let regex = /[aeiou]/g;
+  let replacedString = str.replace(regex,'_');
+  return replacedString;
 };
 
 /* ------------------------------------------------------------------------------------------------
