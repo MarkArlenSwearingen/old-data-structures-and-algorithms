777dc428a2af0a3efedf5ae193722cf25ba77e63
marks <markswearingen@bendcable.com>
2019-05-02 21:41:22 -0700

    challene 9 #2

--- code-challenges/301/09-reduce/challenges-09.test.js
+++ code-challenges/301/09-reduce/challenges-09.test.js
@@ -70,7 +70,11 @@ let starWarsData = [{
 }];
 
 const returnNames = (arr) => {
-  // Solution code here...
+  const names = arr.reduce( (acc, val) => {
+    acc.push( val.name );
+    return acc;
+  }, [] );
+  return names;
 };
 
 /* ------------------------------------------------------------------------------------------------
